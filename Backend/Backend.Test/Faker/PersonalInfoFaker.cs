128bd90d159a6d6a7b0980a82fd1f3cdd7c4e119
Aniketbaral97 <109959526+Aniketbaral97@users.noreply.github.com>
2025-01-30 16:01:45 +0545

    added personal info test

--- Backend/Backend.Test/Faker/PersonalInfoFaker.cs
+++ Backend/Backend.Test/Faker/PersonalInfoFaker.cs
@@ -22,6 +22,7 @@ public static class PersonalInfoFaker
     .RuleFor(x => x.DateOfBirth, y => DateOnly.FromDateTime(y.Person.DateOfBirth));
     return faker.Generate();
     }    
+ 
     public static CreatePersonalInfoDto GetIncompleteCreateModel(){
         
     var faker = new Faker<CreatePersonalInfoDto>()
@@ -35,6 +36,57 @@ public static class PersonalInfoFaker
     .RuleFor(x => x.Nationality, y => y.Lorem.Word())
     .RuleFor(x => x.PhoneNumber, y => y.Person.Personnummer())
     .RuleFor(x => x.Gender, y => Domain.Enums.Gender.Female)
+    .RuleFor(x => x.DateOfBirth, y => DateOnly.FromDateTime(y.Person.DateOfBirth));
+    return faker.Generate();
+    } 
+    public static UpdatePersonalInfoDto GetUpdateModel(){    
+    var faker = new Faker<UpdatePersonalInfoDto>()
+    .RuleFor(x => x.Id, y => Guid.NewGuid())
+    .RuleFor(x => x.Address, y => y.Address.FullAddress())
+    .RuleFor(x => x.ShortText, y => y.Lorem.Sentence())
+    .RuleFor(x => x.Designations, y => y.Lorem.Word())
+    .RuleFor(x => x.Details, y => y.Lorem.Sentences())
+    .RuleFor(x => x.Email, y => y.Internet.Email())
+    .RuleFor(x => x.Firstname, y => y.Name.FirstName())
+    .RuleFor(x => x.Lastname, y => y.Name.LastName())
+    .RuleFor(x => x.Nationality, y => y.Lorem.Word())
+    .RuleFor(x => x.PhoneNumber, y => y.Person.Personnummer())
+    .RuleFor(x => x.Gender, y => Domain.Enums.Gender.Female)
+    .RuleFor(x => x.DateOfBirth, y => DateOnly.FromDateTime(y.Person.DateOfBirth));
+    return faker.Generate();
+    }      
+      
+    public static UpdatePersonalInfoDto GetIncompleteUpdateModel(){    
+    var faker = new Faker<UpdatePersonalInfoDto>()
+    .RuleFor(x => x.Id, y => Guid.NewGuid())
+    .RuleFor(x => x.Address, y => y.Address.FullAddress())
+    .RuleFor(x => x.ShortText, y => y.Lorem.Sentence())
+    .RuleFor(x => x.Designations, y => y.Lorem.Word())
+    .RuleFor(x => x.Details, y => y.Lorem.Sentences())
+    .RuleFor(x => x.Email, y => y.Internet.Email())
+    .RuleFor(x => x.Firstname, y => String.Empty)
+    .RuleFor(x => x.Lastname, y => y.Name.LastName())
+    .RuleFor(x => x.Nationality, y => y.Lorem.Word())
+    .RuleFor(x => x.PhoneNumber, y => y.Person.Personnummer())
+    .RuleFor(x => x.Gender, y => Domain.Enums.Gender.Female)
+    .RuleFor(x => x.DateOfBirth, y => DateOnly.FromDateTime(y.Person.DateOfBirth));
+    return faker.Generate();
+    }   
+
+    public static PersonalInfoDto GetPersonalInfoModel(){    
+    var faker = new Faker<PersonalInfoDto>()
+    .RuleFor(x => x.Id, y => Guid.NewGuid())
+    .RuleFor(x => x.Address, y => y.Address.FullAddress())
+    .RuleFor(x => x.ShortText, y => y.Lorem.Sentence())
+    .RuleFor(x => x.Designations, y => y.Lorem.Word())
+    .RuleFor(x => x.Details, y => y.Lorem.Sentences())
+    .RuleFor(x => x.Email, y => y.Internet.Email())
+    .RuleFor(x => x.Firstname, y => y.Name.FirstName())
+    .RuleFor(x => x.Lastname, y => y.Name.LastName())
+    .RuleFor(x => x.Nationality, y => y.Lorem.Word())
+    .RuleFor(x => x.PhoneNumber, y => y.Person.Personnummer())
+    .RuleFor(x => x.Gender, y => Domain.Enums.Gender.Female)
+    .RuleFor(x => x.WorkAvailabilityStatus, y => Domain.Enums.WorkAvailabilityStatus.Available)
     .RuleFor(x => x.DateOfBirth, y => DateOnly.FromDateTime(y.Person.DateOfBirth));
     return faker.Generate();
     }    
--- Backend/Backend.Test/Services/PersonalInfoTests.cs
+++ Backend/Backend.Test/Services/PersonalInfoTests.cs
@@ -1,3 +1,4 @@
+using Application.DTOs.PersonalInfos;
 using Application.Exceptions;
 using Application.Interfaces;
 using Application.Services.PersonalInfo;
@@ -33,5 +34,74 @@ public class PersonalInfoTests
     {
         var model = PersonalInfoFaker.GetIncompleteCreateModel();
         Assert.That(async ()=> await _service.AddPersonalInfoAsync(model),Throws.Exception.TypeOf<ValidationException>());
+    }
+    [Test]
+    public void UpdatePersonalInfo_WhenSucces_ReturnInt()
+    {
+        var model = PersonalInfoFaker.GetUpdateModel();
+        var repoResult = _repository.Setup(x=>x.CheckIsMain(model.Id)).Returns(false);
+        var repoResult2 = _repository.Setup(x=>x.UpdatePersonalInfoAsync(model)).ReturnsAsync(1);
+        var result =_service.UpdatePersonalInfoAsync(model).GetAwaiter().GetResult();
+        Assert.That(result, Is.EqualTo(1));
+    }
+    [Test]  
+    public void UpdatePersonalInfo_WhenDuplicateIsMain_ThrowCommandExecutionException()
+    {
+        var model = PersonalInfoFaker.GetUpdateModel();
+        model.IsMain=true;
+        var repoResult = _repository.Setup(x=>x.CheckIsMain(model.Id)).Returns(true);
+        Assert.That(async () =>await _service.UpdatePersonalInfoAsync(model), Throws.Exception.TypeOf<CommandExecutionException>());
+    }
+    [Test]
+    public void UpdatePersonalInfo_WhenFluentValidationError_ThrowValidationException()
+    {
+        var model = PersonalInfoFaker.GetIncompleteUpdateModel();
+        model.IsMain=true;
+        var repoResult = _repository.Setup(x=>x.CheckIsMain(model.Id)).Returns(false);
+        Assert.That(async () =>await _service.UpdatePersonalInfoAsync(model), Throws.Exception.TypeOf<ValidationException>());
+    }
+    [Test]
+    public void GetPersonalInfo_WhenSuccess_ReturnPersonalInfo()
+    {
+        var id=Guid.NewGuid();
+        var model = PersonalInfoFaker.GetPersonalInfoModel();
+        var repoResult = _repository.Setup(x=>x.GetPersonalInfoByIdAsync(id)).ReturnsAsync(model);
+        var result =_service.GetPersonalInfoByIdAsync(id).GetAwaiter().GetResult();
+        Assert.That(result, Is.EqualTo(model)); 
+    }
+    [Test]
+    public void GetPersonalInfo_WhenInvalidId_ReturnNull()
+    {
+        var id=Guid.NewGuid();
+        PersonalInfoDto? model = null;
+        var repoResult = _repository.Setup(x=>x.GetPersonalInfoByIdAsync(id)).ReturnsAsync(model);
+        var result =_service.GetPersonalInfoByIdAsync(id).GetAwaiter().GetResult();
+        Assert.That(result, Is.EqualTo(model)); 
+    }
+
+    [Test]
+    public void DeletePersonalInfoById_WhenSuccess_ReturnOne()
+    {
+        var id= Guid.NewGuid();
+        var repoResult=_repository.Setup(x=>x.DeletePersonalInfoAsync(id)).ReturnsAsync(1);
+        var result =_service.DeletePersonalInfoAsync(id).GetAwaiter().GetResult();
+        Assert.That(result, Is.EqualTo(1));
+    }
+    [Test]
+    public void GetPersonalInfoList_WhenSuccess_ReturnList()
+    {
+        var request= new PersonalInfoDemoRequestDto();
+        var expectedResponse = new GetPersonalInfoDemoDto(){
+            TotalPages=1,
+            PersonalInfos=[new PersonalInfoDemo(){
+                Firstname="A",
+                Lastname="B",
+                Designations="C",
+                Address="D",
+            }]
+        };
+        var repoResult=_repository.Setup(x=>x.GetPersonalInfoList(It.IsAny<PersonalInfoDemoRequestDto>())).ReturnsAsync(expectedResponse);
+        var result =_service.GetPersonalInfoList(request).GetAwaiter().GetResult();
+        Assert.That(result, Is.EqualTo(expectedResponse));
     }
 }
